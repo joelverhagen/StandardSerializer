9da6d3a40d7347715e375ee38898d1de675924b1
Joel Verhagen <joel.verhagen@gmail.com>
2014-10-26 23:03:16 -0700

    Add example and update readme

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,28 +1,4 @@
-StandardSerializer
-==================
-
-A contract resolver for Json.NET to make making serializing property names simpler.
-
-.NET properties are typically written in Pascal case (e.g. `MyProperty`). Many JSON properties are written in a
-lowercase, underscore-delimited form (e.g. `my_property`). This leads to headaches when consuming some web API
-endpoints that serve out JSON because the property names don't automatically deserialize to your wonderful C# models.
-
-If you are writing .NET code to consume JSON and you're using [Json.NET][1] (Newtonsoft.Json on NuGet), maybe you've
-got lucky with the default contract serializer... more likely is that you've annotated the heck out of your models with
-[`JsonPropertyAttribute`][2]. This leads to a lot of annoyance and really clouds up your model with mundain details
-like serialization. Bah!
-
-[1]: http://james.newtonking.com/json
-[2]: http://james.newtonking.com/json/help/index.html?topic=html/T_Newtonsoft_Json_JsonPropertyAttribute.htm)
-
-This library tries to give you all the flexibility you need to avoid `JsonPropertyAttribute` in your models while still
-allowing you to consume that juicy JSON.
-
-Example
--------
-
-```csharp
-using System;
+﻿using System;
 using Knapcode.StandardSerializer;
 using Newtonsoft.Json;
 
@@ -69,4 +45,3 @@ namespace Example
         }
     }
 }
-```
---
+++ Example/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Example")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b898f683-3cbe-44c5-a5e4-a96acb314605")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
